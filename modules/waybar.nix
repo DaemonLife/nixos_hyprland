9e7da9a3045d45a1c203b7c08d00ae74aad34b40
DaemonLife <beleevskij21@gmail.com>
2024-10-27 01:42:11 +0300

    Configured waybar clock

--- modules/waybar.nix
+++ modules/waybar.nix
@@ -76,7 +76,9 @@
             disabled = "bt";
           };
           tooltip-format = "{}";
-          on-click = "rfkill toggle bluetooth";
+          # on-click = "rfkill toggle bluetooth";
+          on-click = "rfkill unblock bluetooth && overskride";
+          on-click-right = "rfkill block bluetooth";
         };
 
         "hyprland/language" = {
@@ -86,8 +88,32 @@
         };
 
         clock = {
-          format = "{:%y/%m/%d %H:%M}";
+          # format = "{:%y/%m/%d %H:%M}";
+          format = "{:%H:%M}";
           on-click = "exec gnome-calendar";
+          "tooltip-format" = "<span size='10pt' font='JetBrainsMono Nerd Font Mono'>{calendar}</span>";
+          "calendar" = {
+            "mode"          = "year";
+            "mode-mon-col"  = 3;
+            "weeks-pos"     = "right";
+            "on-scroll"     = 1;
+            "on-click-right"= "mode";
+            "format" = {
+              "months"=     "<span color='#${base07}'><b>{}</b></span>";
+              "days"=       "<span color='#${base0B}'><b>{}</b></span>";
+              "weeks"=      "<span color='#${base0C}'><b>W{}</b></span>";
+              "weekdays"=   "<span color='#${base0A}'><b>{}</b></span>";
+              "today"=      "<span color='#${base08}'><b><u>{}</u></b></span>";
+            };
+          };
+          "actions"= {
+            "on-click-right"= "mode";
+            "on-click-forward"= "tz_down";
+            "on-click-backward"= "tz_up";
+            "on-scroll-up"= "shift_down";
+            "on-scroll-down"= "shift_up";
+        };
+
         };
 
         battery = {
@@ -111,8 +137,8 @@
           format-ethernet = " {ipaddr}/{cidr}";
           format-linked = " (No IP)";
           format-disconnected = "wifi";
-          on-click = ''kitty --hold sh -c "nmcli dev wifi rescan && nmtui"'';
-          on-click-right = "rfkill toggle wifi";
+          on-click = ''rfkill unblock wifi && kitty --hold sh -c "nmcli dev wifi rescan && nmtui"'';
+          on-click-right = "rfkill block wifi";
         };
 
         pulseaudio = {
@@ -246,6 +272,14 @@
         }
         window#waybar {
           background-color: @dark;
+        }
+
+        /* calendar look */
+        tooltip {
+          background-color: #${base01};
+          border: 2px;
+          border-style: solid;
+          border-color: @accent;
         }
 
       	#network.disconnected { color: @red;}
